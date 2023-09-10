af833b128349e04c0b8d77be714ec68d46c00c61
Andrei Antal <antal.andrei@icloud.com>
2023-09-10 20:47:41 +0300

    done challenge 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MoviesModule } from './movies/movies.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, MoviesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
