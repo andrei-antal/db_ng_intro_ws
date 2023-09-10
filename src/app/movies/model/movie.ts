5ff58a1882a1ef2398f433b9c24858572a7f34ac
Andrei Antal <antal.andrei@icloud.com>
2023-09-10 20:51:31 +0300

    done challenge 9

---
+++ src/app/movies/model/movie.ts
@@ -0,0 +1,9 @@
+export interface Movie {
+  id: string;
+  title: string;
+  year: string;
+  genre: string;
+  plot: string;
+  comment: string;
+  poster?: string;
+}
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MovieItemComponent } from './components/movie-item/movie-item.component';
+import { MovieListComponent } from './components/movie-list/movie-list.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule],
-  declarations: [MovieItemComponent],
-  exports: [MovieItemComponent],
+  declarations: [MovieItemComponent, MovieListComponent],
+  exports: [MovieItemComponent, MovieListComponent],
 })
 export class MoviesModule {}
