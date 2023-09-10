54fb793523192efc8dec0377614134da04dbabf8
Andrei Antal <antal.andrei@icloud.com>
2023-09-10 20:48:54 +0300

    done challenge 3

--- src/app/movies/components/movie-item/movie-item.component.ts
+++ src/app/movies/components/movie-item/movie-item.component.ts
@@ -5,4 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './movie-item.component.html',
   styleUrls: ['./movie-item.component.scss'],
 })
-export class MovieItemComponent {}
+export class MovieItemComponent {
+  title = 'Fight Club';
+  year = '1999';
+  genre = 'Mystery, Thriller, Drama';
+  plot =
+    "A depressed man (Edward Norton) suffering from insomnia meets a strange soap salesman named Tyler Durden (Brad Pitt) and soon finds himself living in his squalid house after his perfect apartment is destroyed. The two bored men form an underground club with strict rules and fight other men who are fed up with their mundane lives. Their perfect partnership frays when Marla (Helena Bonham Carter), a fellow support group crasher, attracts Tyler's attention.";
+  poster =
+    'https://resizing.flixster.com/0kbkzWG-fGf5yEZSmLw4VB_SpnQ=/206x305/v2/https://flxt.tmsimg.com/assets/p23069_p_v8_aa.jpg';
+}
