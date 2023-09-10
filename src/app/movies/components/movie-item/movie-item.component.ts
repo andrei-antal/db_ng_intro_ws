76cd5f1b8952d70f80739b3067a250f2fbfa44bc
Andrei Antal <antal.andrei@icloud.com>
2023-09-10 20:49:48 +0300

    done challenge 5

--- src/app/movies/components/movie-item/movie-item.component.ts
+++ src/app/movies/components/movie-item/movie-item.component.ts
@@ -13,4 +13,14 @@ export class MovieItemComponent {
     "A depressed man (Edward Norton) suffering from insomnia meets a strange soap salesman named Tyler Durden (Brad Pitt) and soon finds himself living in his squalid house after his perfect apartment is destroyed. The two bored men form an underground club with strict rules and fight other men who are fed up with their mundane lives. Their perfect partnership frays when Marla (Helena Bonham Carter), a fellow support group crasher, attracts Tyler's attention.";
   poster =
     'https://resizing.flixster.com/0kbkzWG-fGf5yEZSmLw4VB_SpnQ=/206x305/v2/https://flxt.tmsimg.com/assets/p23069_p_v8_aa.jpg';
+
+  movieComment = '';
+
+  wordCount(comment: string): number {
+    if (!comment || comment.length === 0) {
+      return 0;
+    } else {
+      return comment.trim().replace(/  +/g, ' ').split(' ').length;
+    }
+  }
 }
--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MovieItemComponent } from './components/movie-item/movie-item.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   declarations: [MovieItemComponent],
   exports: [MovieItemComponent],
 })
